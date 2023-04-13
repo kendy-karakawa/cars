6bd710b656ecb45c35de9a9d646b49501dad1cf5
kendy-karakawa <kendy.karakawa@gmail.com>
2023-04-12 21:00:16 -0300

    first commit

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,35 @@
+import { Prisma } from "@prisma/client";
+import prisma from "../config/database.js";
+
+async function getCars() {
+  const data = await prisma.cars.findMany()
+  return data;
+}
+
+async function getCar(id: number) {
+  const data = await prisma.cars.findUnique({ where:{id }})
+  return data;
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  const data = await prisma.cars.findUnique({where:{licensePlate}})
+  return data;
+}
+
+async function createCar(model: string, licensePlate: string, year: number, color: string) {
+  await prisma.cars.create({data:{model, licensePlate, year, color}})
+}
+
+async function deleteCar(id: number) {
+  await prisma.cars.delete({where:{id}})
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar
+}
+
+export default carRepository;
